fcb42d1d019f3de7d3fab0872f20ed529bb74d42
Anonymous <anonymous@anonymous.anonymous>
2021-04-30 13:37:31 +0100

    Initial commit: setting up the project

---
+++ src/main/java/com/digitalstork/tictactoespring/TictactoespringApplication.java
@@ -0,0 +1,13 @@
+package com.digitalstork.tictactoespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoespringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalstork/tictactoespring/TictactoespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalstork.tictactoespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
