72048d7c185927b9227babccbd670f455251dd73
Anonymous <anonymous@anonymous.anonymous>
2021-04-30 17:44:34 +0100

    feat: implementing the createBoard method of the BoardServiceImpl Class

--- src/main/java/com/digitalstork/tictactoespring/service/BoardServiceImpl.java
+++ src/main/java/com/digitalstork/tictactoespring/service/BoardServiceImpl.java
@@ -1,15 +1,24 @@
 package com.digitalstork.tictactoespring.service;
 
 import com.digitalstork.tictactoespring.dto.BoardDTO;
+import com.digitalstork.tictactoespring.mapper.BoardMapper;
+import com.digitalstork.tictactoespring.model.Board;
+import com.digitalstork.tictactoespring.repository.BoardRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BoardServiceImpl implements BoardService{
 
 
+    private BoardRepository boardRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(BoardServiceImpl.class);
+    private static final BoardMapper BOARD_MAPPER = new BoardMapper();
+
 
     @Override
     public BoardDTO createBoard() {
-        return null;
+        var board = boardRepository.save(new Board());
+        LOG.info("Initial game: {}\n", board.drawBoard()!= null ? board.drawBoard(): "");
+        return BOARD_MAPPER.apply(board);
     }
-
-
 }
