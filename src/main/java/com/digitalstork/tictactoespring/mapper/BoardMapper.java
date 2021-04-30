3ec32a2274eec9294e43b27af3d05f477472c59a
Anonymous <anonymous@anonymous.anonymous>
2021-04-30 14:54:36 +0100

    feat: implementation of the apply method of the MapperBoard class

--- src/main/java/com/digitalstork/tictactoespring/mapper/BoardMapper.java
+++ src/main/java/com/digitalstork/tictactoespring/mapper/BoardMapper.java
@@ -2,12 +2,18 @@ package com.digitalstork.tictactoespring.mapper;
 
 import com.digitalstork.tictactoespring.dto.BoardDTO;
 import com.digitalstork.tictactoespring.model.Board;
+import com.digitalstork.tictactoespring.model.enumeration.Box;
 
 import java.util.function.Function;
 
 public class BoardMapper implements Function<Board, BoardDTO> {
 
     public BoardDTO apply(Board board) {
-        return new BoardDTO();
+        String nextPlayer = board.getNextPlayer() == Box.BLANK ? "X can start the game" : String.format("%s Player", board.getNextPlayer());
+        return BoardDTO.builder()
+                .id(board.getId())
+                .endBoard(board.isEndBoard())
+                .nextPlayer(nextPlayer)
+                .build();
     }
 }
